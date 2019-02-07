8dd122bedd08f7ae2c92eca4a58e842f137b14ab
Travis Cao <scaotravis@gmail.com>
2019-02-07 14:48:45 -0600

    implement two more tests for Book class

--- P03 Book Library/src/BookLibraryTests.java
+++ P03 Book Library/src/BookLibraryTests.java
@@ -2,7 +2,7 @@
 public class BookLibraryTests {
 
   /**
-   * Checks that the constructor of Book class correctly initializes all instances fields for a new
+   * Checks that the constructor of Book class correctly initializes all instance fields for a new
    * Book object; this includes title, author, borrowerCardBarCode, ID (this test also implicitly
    * checks the increments nextID static field)
    * 
@@ -12,21 +12,21 @@ public class BookLibraryTests {
     boolean testPassed = true; // boolean local variable evaluated to true if this test passed,
                                // false otherwise
 
-    // checks constructing the first book yields correct instances fields
+    // checks constructing the first book yields correct instance fields
     Book b1 = new Book("A Title", "An Author");
     if (b1.getTitle() != "A Title" || b1.getAuthor() != "An Author"
         || b1.getBorrowerCardBarCode() != null || b1.getID() != 1) {
       testPassed = false;
     }
 
-    // creates the second book; checks all instances fields
+    // creates the second book; checks all instance fields
     Book b2 = new Book();
     if (b2.getTitle() != null || b2.getAuthor() != null || b2.getBorrowerCardBarCode() != null
         || b2.getID() != 2) {
       testPassed = false;
     }
 
-    // creates the third book; checks all instances fields
+    // creates the third book; checks all instance fields
     Book b3 = new Book("Another Title", "Another Author");
     if (b3.getTitle() != "Another Title" || b3.getAuthor() != "Another Author"
         || b3.getBorrowerCardBarCode() != null || b3.getID() != 3) {
@@ -58,18 +58,64 @@ public class BookLibraryTests {
       testPassed = false;
     }
 
+    // checks that, after borrowing the book, the .getBorrowerCardBarCode() method should return the
+    // correct borrowerCardBarCode
+    book.borrowBook(12345);
+
+    if (book.getBorrowerCardBarCode() != 12345) {
+      testPassed = false;
+    }
+
     return testPassed;
   }
 
 
   /**
-   * Checks whether returnBook() method defined within your Book class sets correctly the instance
-   * field borrowerCardBarCode
+   * Checks whether returnBook() in the Book class sets correctly the instance field
+   * borrowerCardBarCode
    * 
    * @return true if the test passed, false otherwise
    */
   public static boolean testBookReturnBook() {
-    boolean testPassed = false;
+    boolean testPassed = true; // boolean local variable evaluated to true if this test passed,
+                               // false otherwise
+
+    Book book = new Book("TITLE", "AUTHOR");
+
+    // sets the book to be borrowed by borrowerCardBarCode 123456
+    book.borrowBook(123456);
+
+    // returns the book, and checks that borrowerCardBarCode should now be set to null to indicate
+    // that the book is available
+    book.returnBook();
+    if (book.getBorrowerCardBarCode() != null) {
+      testPassed = false;
+    }
+
+    return testPassed;
+  }
+
+  public static boolean testIsAvailable() {
+    boolean testPassed = true;
+
+    Book book = new Book("TITLE", "AUTHOR");
+    
+    // book now has yet to be borrowed by anyone; checks that book is available
+    if (book.isAvailable() != true) {
+      testPassed = false; 
+    }
+
+    // sets the book to be borrowed; checks that book now is not available
+    book.borrowBook(123456);
+    if (book.isAvailable() != false) {
+      testPassed = false; 
+    }
+    
+    // returns the book; checks that book now is available again
+    book.returnBook();
+    if (book.isAvailable() != true) {
+      testPassed = false; 
+    }
 
     return testPassed;
   }
@@ -78,6 +124,8 @@ public class BookLibraryTests {
   public static void main(String[] args) {
     System.out.println(testBookConstructor());
     System.out.println(testBookGetters());
+    System.out.println(testBookReturnBook());
+    System.out.println(testIsAvailable());
   }
 
 }
