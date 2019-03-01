06a32d1d9c3c83c6a83918f7504caeed94cc5d09
Travis Cao <scaotravis@gmail.com>
2019-03-01 16:17:16 -0600

    P05: complete step 2

--- P05 Escape Room/src/EscapeRoom.java
+++ P05 Escape Room/src/EscapeRoom.java
@@ -1,9 +1,27 @@
-import processing.core.PApplet; 
+import processing.core.PApplet;
+import processing.core.PImage;
 
 public class EscapeRoom extends PApplet {
-  
-  public static void main (String[] args) {
-    PApplet.main("EscapeRoom"); 
+
+  private PImage backgroundImage;
+
+  @Override
+  public void settings() {
+    size(800, 600);
   }
-  
+
+  @Override
+  public void setup() {
+    backgroundImage = super.loadImage("images/computerCenter.png");
+  }
+
+  @Override
+  public void draw() {
+    super.image(backgroundImage, 0f, 0f);
+  }
+
+  public static void main(String[] args) {
+    PApplet.main("EscapeRoom");
+  }
+
 }
