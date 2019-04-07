f815d29dbfd8b803d8ae8bd3106ce03cc286f6f1
Travis Cao <scaotravis@gmail.com>
2019-04-06 20:17:22 -0500

    P08: 3.1 using ServingQueue object

--- P08 Dessert Queue/src/DessertSolvers.java
+++ P08 Dessert Queue/src/DessertSolvers.java
@@ -9,11 +9,11 @@ public class DessertSolvers {
   /**
    * Solve to whom the dessert should be first served, which is the same person who got last served
    * for meals. The meal serving starts with the first arrival, then skip guestsSkipped people, and
-   * serve the arrival right after
+   * serve the arrival right after the skipped one
    * 
    * @param numberOfGuests Total number of guests arrived at the dinner party
-   * @param guestsSkipped  Number of guests to skip in between two meal serving rounds
-   * @return The Guest who will first get the dessert served (i.e. who got served meal the last)
+   * @param guestsSkipped  Number of guests to skip in between two meal servings
+   * @return The Guest who will first get the dessert served
    */
   public static Guest firstDessertVariableSkips(int numberOfGuests, int guestsSkipped) {
     if (numberOfGuests <= 0) {
@@ -23,30 +23,27 @@ public class DessertSolvers {
       throw new IllegalArgumentException("Number of guests skipped is not non-negative.");
     }
 
-    Guest[] arrivalQueue = new Guest[numberOfGuests];
+    ServingQueue queue = new ServingQueue(numberOfGuests);
+
+    // create a queue of guests based on their arrival
     Guest.resetNextGuestIndex();
     for (int i = 0; i < numberOfGuests; i++) {
-      arrivalQueue[i] = new Guest();
+      queue.add(new Guest());
     }
 
-    int indexToServe = 0; // which guest to serve next
+    // remove guests from the queue if they have been served, otherwise remove them and then add it
+    // back to the queue
     int totalServed = 0; // how many in total have been served
-    int skipCounter = 0; // how many times have we skipped guests
-
     while (totalServed != numberOfGuests - 1) {
-      arrivalQueue[indexToServe] = null;
+      queue.remove();
       totalServed++;
 
-      skipCounter = 0;
-      while (skipCounter != guestsSkipped + 1) { // skip till reaching the next person to serve
-        indexToServe = (indexToServe + 1) % numberOfGuests;
-        if (arrivalQueue[indexToServe] != null) {
-          skipCounter++;
-        }
+      for (int i = 0; i < guestsSkipped; i++) {
+        queue.add(queue.remove());
       }
     }
 
-    return arrivalQueue[indexToServe];
+    return queue.remove();
   }
 
   public static void main(String[] args) {
