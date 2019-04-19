7e29b25b50ac9db57a4772e6f1c11a4ba70599da
Travis Cao <scaotravis@gmail.com>
2019-04-19 11:23:13 -0500

    P10: initializes step 1 SupportTicket class

---
+++ P10 Help Desk/src/SupportTicket.java
@@ -0,0 +1,22 @@
+
+/**
+ * Models a support ticket received by the help desk
+ * 
+ * @author Travis Cao
+ */
+public class SupportTicket implements Comparable<SupportTicket> {
+
+  
+  // Methods
+  /**
+   * Compares the priority of tickets in order to later prioritize
+   * 
+   * @param o A different SupportTicket to compare priority with
+   * @return 
+   */
+  public int compareTo(SupportTicket o) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+}
