52eb46667afc38bd3874a52b6ff4a166a569ec70
Travis Cao <scaotravis@gmail.com>
2020-05-20 16:15:08 -0500

    clean up header style

--- P02 Particle Fountain/src/Fountain.java
+++ P02 Particle Fountain/src/Fountain.java
@@ -1,43 +1,6 @@
-//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
-//
-// Title: P02 Particle Fountain
-// Files: "Fountain.java"
-// Course: CS 300, Spring, 2019
-//
-// Author: Travis Cao
-// Email: travis.cao@wisc.edu
-// Lecturer's Name: Gary Dahl
-//
-//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
-//
-// Partner Name: NONE
-// Partner Email: N/A
-// Partner Lecturer's Name: N/A
-//
-// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
-// ___ Write-up states that pair programming is allowed for this assignment.
-// ___ We have both read and understand the course Pair Programming Policy.
-// ___ We have registered our team prior to the team registration deadline.
-//
-///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
-//
-// Students who get help from sources other than their partner must fully
-// acknowledge and credit those sources of help here. Instructors and TAs do
-// not need to be credited here, but tutors, friends, relatives, room mates,
-// strangers, and others do. If you received no outside help from either type
-// of source, then please explicitly indicate NONE.
-//
-// Persons: NONE
-// Online Sources:
-// 1. Guide for Random class
-// https://docs.oracle.com/javase/8/docs/api/java/util/Random.html
-// 2. Guide for Utility and Particle classes
-// http://cs300-www.cs.wisc.edu/wp/wp-content/uploads/2019/01/p2javadocs/
-// 3. Stack Overflow question 40431966: how to generate random number within
-// certain range
-// https://stackoverflow.com/questions/40431966/
-//
-/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
+/**
+ * @author Travis Cao
+ */
 
 import java.util.Random;
 
--- P08 Dessert Queue/src/QueueTests.java
+++ P08 Dessert Queue/src/QueueTests.java
@@ -1,37 +1,3 @@
-//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
-//
-// Title: P08 Dessert Queue
-// Files: "Guest.java", "ServingQueue.java", "QueueTests.java",
-// "DessertSolvers.java"
-// Course: CS 300, Spring, 2019
-//
-// Author: Travis Cao
-// Email: travis.cao@wisc.edu
-// Lecturer's Name: Gary Dahl
-//
-//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
-//
-// Partner Name: NONE
-// Partner Email: N/A
-// Partner Lecturer's Name: N/A
-//
-// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
-// ___ Write-up states that pair programming is allowed for this assignment.
-// ___ We have both read and understand the course Pair Programming Policy.
-// ___ We have registered our team prior to the team registration deadline.
-//
-///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
-//
-// Students who get help from sources other than their partner must fully
-// acknowledge and credit those sources of help here. Instructors and TAs do
-// not need to be credited here, but tutors, friends, relatives, room mates,
-// strangers, and others do. If you received no outside help from either type
-// of source, then please explicitly indicate NONE.
-//
-// Persons: NONE
-// Online Sources: NONE
-//
-/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
 
 /**
  * Test serving a queue of guests
